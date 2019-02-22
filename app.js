68d5538ab4dc8601c97f470e1a3798d68b259fb4
Bharath-RajG <bharath958@gmail.com>
2019-02-22 18:33:10 +0530

    'changes'

--- app.js
+++ app.js
@@ -18,28 +18,28 @@ const app = express();
 
 /*middleware.Parse incoming request bodies in a middleware before your handlers*/
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 
 /* USE Session */
 app.use(session({
-    secret:'my secret',
+    secret: 'my secret',
     resave: false,
-    saveUninitialized:false,
-    cookie:{maxAge:1000000}
+    saveUninitialized: false,
+    cookie: { maxAge: 1000000 }
 }))
 
 
 app.use(validtor());
 
-app.set('views',__dirname+'/views');
-app.set('view engine','hbs');
-hbs.registerPartials(__dirname+'/views/partials');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 /*index value increment in project details*/
-hbs.registerHelper('increment', function(value, options){
-    return value+1;
+hbs.registerHelper('increment', function (value, options) {
+    return value + 1;
 });
 
-app.use(express.static(__dirname+'/static'))
+app.use(express.static(__dirname + '/static'))
 /*middleWare*/
 app.use(appMiddle.logger);
 app.use(appMiddle.authenticated);
@@ -47,9 +47,9 @@ app.use(appMiddle.authenticated);
 app.use('/', index);
 app.use('/projects', project);
 app.use('/blogs', blog);
-app.use('/admin', appMiddle.authenticate , admin);
+app.use('/admin', appMiddle.authenticate, admin);
 
 app.use(appMiddle.notFoundError);
 app.use(appMiddle.handleError);
 
-app.listen(3002, ()=> console.log('Server started on port 3002'));
+app.listen(3002, () => console.log('Server started on port 3002'));
